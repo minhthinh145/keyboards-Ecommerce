c04d409c51a3d90acda14508b5c92198ca8a70ce
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:15:51 +0700

    Feat(UserOtp) : Create Model and DTO  of UserOtp

---
+++ backend/KeyBoard/DTOs/AuthenDTOs/CreateUserOtpDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace KeyBoard.DTOs.AuthenDTOs
+{
+    public class CreateUserOtpDTO
+    {
+        public string UserId { get; set; }
+        public string OtpCode { get; set; }
+        public DateTime ExpirationTime { get; set; }
+    }
+}
---
+++ backend/KeyBoard/Data/UserOTP.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KeyBoard.Data
+{
+    public class UserOTP
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string OtpCode{ get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpirationTime { get; set; }
+        public bool IsUsed { get; set; } = false;
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+
+    }
+}
