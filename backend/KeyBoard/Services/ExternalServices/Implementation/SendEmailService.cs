d139277d700e8943eb9d7ca9fedf944af472acb7
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:20:05 +0700

    feat: add interface and implement for send OTP by email service

---
+++ backend/KeyBoard/Services/ExternalServices/Implementation/SendEmailService.cs
@@ -0,0 +1,40 @@
+﻿using KeyBoard.Services.ExternalServices.Interface;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+namespace KeyBoard.Services.ExternalServices.Implementation
+{
+    public class SendEmailService : ISendEmailService
+    {
+        private readonly IConfiguration _configuration;
+        public SendEmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendOtpEmailAsync(string toEmail, string otpCode)
+        {
+            var message = new MimeKit.MimeMessage();
+            message.From.Add(new MimeKit.MailboxAddress("Hệ thống bán bàn phím", _configuration["EmailSettings:From"]));
+            message.To.Add(MailboxAddress.Parse(toEmail));
+            message.Subject = "Xác thực OTP - Website bán bàn phím";
+            message.Body = new TextPart("plain")
+            {
+                Text = $"Mã OTP của bạn là: {otpCode}. Vui lòng không chia sẻ mã này với bất kỳ ai. Mã sẽ hết hạn sau vài phút."
+            };
+            using var client = new SmtpClient();
+            await client.ConnectAsync(
+                _configuration["EmailSettings:SmtpServer"],
+                int.Parse(_configuration["EmailSettings:Port"]),
+                true 
+            );
+
+            await client.AuthenticateAsync(
+                 _configuration["EmailSettings:Username"],
+                 _configuration["EmailSettings:Password"]
+             );
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ backend/KeyBoard/Services/ExternalServices/Interface/ISendEmailService.cs
@@ -0,0 +1,12 @@
+﻿namespace KeyBoard.Services.ExternalServices.Interface
+{
+    public interface ISendEmailService
+    {
+        /// <summary>
+        /// Send OTP to User
+        /// </summary>
+        /// <param name="toEmail">Email người nhận</param>
+        /// <param name="otpCode">Mã OTP cần gửi</param>
+        Task SendOtpEmailAsync(string toEmail, string otpCode);
+    }
+}
