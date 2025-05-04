ce9db0533af14cc35762a69e389e3d32f6342f56
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:21:49 +0700

    feat feat: add ChangePassword service interface and implementation

---
+++ backend/KeyBoard/Services/Implementations/ChangePasswordService.cs
@@ -0,0 +1,65 @@
+﻿using KeyBoard.Data;
+using KeyBoard.DTOs.AuthenDTOs;
+using KeyBoard.Helpers;
+using KeyBoard.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace KeyBoard.Services.Implementations
+{
+    public class ChangePasswordService : IChangePasswordService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IPasswordValidator<ApplicationUser> _passwordValidator;
+        private readonly IUserOtpService _OtpService;
+
+        public ChangePasswordService(UserManager<ApplicationUser> userManager, IPasswordValidator<ApplicationUser> passwordValidator , IUserOtpService otpService) 
+        {
+            _userManager = userManager;
+            _passwordValidator = passwordValidator;
+            _OtpService = otpService;
+        }
+        public async Task<ServiceResult> ConfirmChangePasswordAsync(ConfirmChangePasswordDTO ConfirmChangePasswordDTO)
+        {
+           var user = await _userManager.FindByIdAsync(ConfirmChangePasswordDTO.UserId);
+            if (user == null)
+            {
+                return ServiceResult.Failure("Không tìm thấy User");
+            }
+           var validateResult = await _passwordValidator.ValidateAsync(_userManager ,user, ConfirmChangePasswordDTO.NewPassword);
+            if (!validateResult.Succeeded)
+            {
+                var errors = string.Join("; ", validateResult.Errors.Select(e => e.Description));
+                return  ServiceResult.Failure($"Mật khẩu mới không hợp lệ: {errors}");  
+            }
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var result = await _userManager.ResetPasswordAsync(user, token, ConfirmChangePasswordDTO.NewPassword);
+            if(!result.Succeeded)
+            {
+                var errors = string.Join("; ", result.Errors.Select(e => e.Description));
+                return ServiceResult.Failure($"Đổi mật khẩu không thành công: {errors}");
+            }
+            //mark otp is used
+            var otpResult = await _OtpService.MarkOtpAsUsedAsync(ConfirmChangePasswordDTO.UserId,ConfirmChangePasswordDTO.OtpCode);
+            if (!otpResult)
+            {
+                return ServiceResult.Failure("Mã OTP không hợp lệ hoặc đã được sử dụng");
+            }
+            return ServiceResult.Success("Đổi mật khẩu thành công");
+        }
+
+        public async Task<ServiceResult> RequestChangePasswordAsync(ChangePasswordDTO changePasswordDTO)
+        {
+            var user = await _userManager.FindByIdAsync(changePasswordDTO.UserId);
+            if (user == null)
+            {
+                return ServiceResult.Failure("Không tìm thấy User");
+            }
+            if(changePasswordDTO.NewPassword != changePasswordDTO.ConfirmPassword)
+            {
+                return ServiceResult.Failure("Mật khẩu không khớp, vui lòng nhập lại!");
+            }
+
+            return ServiceResult.Success("Vui lòng chọn phương thức gửi mã OTP");
+        }
+    }
+}
---
+++ backend/KeyBoard/Services/Interfaces/IChangePasswordService.cs
@@ -0,0 +1,22 @@
+﻿using KeyBoard.DTOs.AuthenDTOs;
+using KeyBoard.Helpers;
+
+namespace KeyBoard.Services.Interfaces
+{
+    public interface IChangePasswordService
+    {
+        /// <summary>
+        ///  Checks the password change request and ask User to choice method to receive OTP
+        /// </summary>
+        /// <param name="changePasswordDTO">Data including UserId , newPassword and confirmPassword</param>
+        /// <returns>Notification of error ( if false )</returns>
+        Task<ServiceResult> RequestChangePasswordAsync(ChangePasswordDTO changePasswordDTO);
+
+        /// <summary>
+        /// Verify OTP and Change Password if vaild
+        /// </summary>
+        /// <param name="verifyOtpAndChangePasswordDTO">Data including UserId , OTP code and newPassword </param>
+        /// <returns>A confirm message or an error message if applicable </returns>
+        Task<ServiceResult> ConfirmChangePasswordAsync(ConfirmChangePasswordDTO ConfirmChangePasswordDTO);
+    }
+}
