fa5ca6db0785dc7327b9eb909dce9f25796ac833
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:19:22 +0700

    feat: implement ServiceResult for consistent Service responses

---
+++ backend/KeyBoard/Helpers/ServiceResult.cs
@@ -0,0 +1,19 @@
+﻿namespace KeyBoard.Helpers
+{
+    public class ServiceResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public static ServiceResult Success(string message = "", object data = null)
+        {
+            return new ServiceResult { IsSuccess = true, Message = message, Data = data };
+        }
+
+        public static ServiceResult Failure(string message = "", object data = null)
+        {
+            return new ServiceResult { IsSuccess = false, Message = message, Data = data };
+        }
+    }
+}
