6941cb4a7b2530253cce75bc6a433ed93188b28d
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:17:24 +0700

    Feat : add ChangePasswordDTO and ConfrimChange for future changePassword service

---
+++ backend/KeyBoard/DTOs/AuthenDTOs/ChangePasswordDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KeyBoard.DTOs.AuthenDTOs
+{
+    public class ChangePasswordDTO
+    {
+        [Required(ErrorMessage = "User ID is required")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "New password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ backend/KeyBoard/DTOs/AuthenDTOs/ConfirmChangePasswordDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KeyBoard.DTOs.AuthenDTOs
+{
+    public class ConfirmChangePasswordDTO
+    {
+        public string UserId { get; set; }
+
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters")]
+        public string NewPassword { get; set; }
+        public string OtpCode { get; set; }
+    }
+}
+
