b8d7aa1f2be55e7a8f83a8c710ce12ecf11e2128
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:21:13 +0700

    feat: add UserOtp repository interface and implementation

---
+++ backend/KeyBoard/Repositories/Implementations/UserOtpRepository.cs
@@ -0,0 +1,41 @@
+﻿using KeyBoard.Data;
+using KeyBoard.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyBoard.Repositories.Implementations
+{
+    public class UserOtpRepository : IUserOtpRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserOtpRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<UserOTP> CreateOTPAsync(UserOTP userDTO)
+        {
+            await _context.UserOtps.AddAsync(userDTO);
+            await _context.SaveChangesAsync();
+            return userDTO;
+        }
+
+        public async Task<UserOTP> GetOtpByCodeAsync(string otpCode)
+        {
+            return await _context.UserOtps
+                .FirstOrDefaultAsync(u => u.OtpCode == otpCode && !u.IsUsed && u.ExpirationTime > DateTime.UtcNow);
+        }
+
+        public async Task<bool> MarkOtpAsUsedAsync(string userId, string otpCode)
+        {
+           var otp = await _context.UserOtps
+                .FirstOrDefaultAsync(o => o.OtpCode == otpCode && o.UserId == userId);
+            if (otp != null)
+            {
+                otp.IsUsed = true;
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+    }
+}   
---
+++ backend/KeyBoard/Repositories/Interfaces/IUserOtpRepository.cs
@@ -0,0 +1,30 @@
+﻿using KeyBoard.Data;
+
+namespace KeyBoard.Repositories.Interfaces
+{
+    public interface IUserOtpRepository
+    {
+        /// <summary>
+        /// Creates a new OTP (One-Time Password) for a user.
+        /// </summary>
+        /// <param name="userDTO">DTO from frontend</param>
+        /// <returns>UserOTP if success</returns>
+        Task<UserOTP> CreateOTPAsync(UserOTP userDTO);
+
+        /// <summary>
+        /// Retrieves an OTP by its code.
+        /// </summary>
+        /// <param name="otpCode"></param>
+        /// <returns>UserOTP if sucess</returns>
+        Task<UserOTP> GetOtpByCodeAsync(string otpCode);
+
+        /// <summary>
+        /// Retrieves an OTP by its ID.
+        /// </summary>
+        /// <param name="otpCode"></param>
+        /// <returns>true if otp is used else false</returns>
+        Task<bool> MarkOtpAsUsedAsync(string userId,string otpCode);
+
+
+    }
+}
