ee5f7f036e565e6c6b00f936940c077d7ee1bda1
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:17:54 +0700

    feat : Create RequestOTP and verifyDTO for future Send/Verify OTP service

---
+++ backend/KeyBoard/DTOs/AuthenDTOs/RequestOtpDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace KeyBoard.DTOs.AuthenDTOs
+{
+    public enum OtpDeliveryMethod
+    {
+        Email,
+        Phone
+    }
+    public class RequestOtpDTO
+    {
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Delivery method is required")]
+        public OtpDeliveryMethod Method { get; set; }
+    }
+}
---
+++ backend/KeyBoard/DTOs/AuthenDTOs/VerifyOtpDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace KeyBoard.DTOs.AuthenDTOs
+{
+    public class VerifyOtpDTO
+    {
+        public string UserId { get; set; }
+        public string OtpCode { get; set; }
+    }
+}
