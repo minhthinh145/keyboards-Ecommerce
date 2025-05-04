e80db063fba348eea737bd32a378dcef7e068ecc
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-04 10:22:20 +0700

    feat: Add mapper for DTO

--- backend/KeyBoard/Helpers/ApplicationMapper.cs
+++ backend/KeyBoard/Helpers/ApplicationMapper.cs
@@ -33,6 +33,19 @@ namespace KeyBoard.Helpers
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Phone))
                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.BirthDate));
 
+            // Ánh xạ CreateUserOtpDTO → UserOTP
+            CreateMap<CreateUserOtpDTO, UserOTP>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString()))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(dest => dest.IsUsed, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore()); // Bỏ qua navigation property
+
+            CreateMap<RequestOtpDTO, CreateUserOtpDTO>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.OtpCode, opt => opt.Ignore()) // OtpCode sẽ được tạo bởi backend
+                .ForMember(dest => dest.ExpirationTime, opt => opt.Ignore()); // ExpirationTime sẽ được tạo bởi backend
+
+        
         }
     }
 }
