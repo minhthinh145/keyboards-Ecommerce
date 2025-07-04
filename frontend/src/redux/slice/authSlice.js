dba65a61a66ea47cd2f572527a26562ed51e09d4
minhthinh145 <minhthinh0384360149@gmail.com>
2025-07-04 19:58:53 +0700

    feat: add fetchCart when user login

--- frontend/src/redux/slice/authSlice.js
+++ frontend/src/redux/slice/authSlice.js
@@ -1,11 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { login as loginApi } from '../../api/auth/login.js';
 import { getProfile } from '@/api/auth/profile.js';
+import { fetchCart } from './cartSlice.js';
 
 //async thunk for login
 export const login = createAsyncThunk(
   'auth/login',
-  async ({ email, password }, { rejectWithValue }) => {
+  async ({ email, password }, { rejectWithValue, dispatch }) => {
     try {
       const { accessToken, refreshToken } = await loginApi(email, password);
       const user = await getProfile(accessToken);
@@ -13,6 +14,8 @@ export const login = createAsyncThunk(
       localStorage.setItem('accessToken', accessToken);
       localStorage.setItem('refreshToken', refreshToken);
       localStorage.setItem('user', JSON.stringify(user));
+
+      dispatch(fetchCart());
       return { accessToken, refreshToken, user };
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -65,6 +68,8 @@ const authSlice = createSlice({
         state.loading = false;
         state.accessToken = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
+        state.user = action.payload.user;
+        //fetchcart
       })
       .addCase(login.rejected, (state, action) => {
         state.loading = false;
